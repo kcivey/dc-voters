65bd6c7074e9d438516d3814c25c7f6e07df3d17
Keith C. Ivey <keith@iveys.org>
2012-08-11 11:46:21 -0400

    allow name and address searches

--- routes/index.js
+++ routes/index.js
@@ -7,18 +7,53 @@ module.exports = function (app) {
 
         search: function (req, res) {
             var q = req.query.q,
-                match = 'MATCH (firstname, lastname, res_house, res_street) AGAINST (? IN BOOLEAN MODE)',
-                sql, values;
+                name = req.query.name,
+                address = req.query.address,
+                values = [],
+                match, sql, m;
             if (q) {
-                sql = 'SELECT *, ' + match + ' AS score FROM voters WHERE ' + match + ' ORDER BY score DESC LIMIT 5';
-                values = [q, q];
-                db.query(sql, values, function (err, results) {
-                    if (err) {
-                        throw err;
-                    }
-                    res.json(results);
-                });
+                match = 'MATCH (firstname, lastname, res_house, res_street) AGAINST (? IN BOOLEAN MODE)';
             }
+            sql = 'SELECT *';
+            if (match) {
+                sql += ', ' + match + ' AS score';
+                values.push(q);
+            }
+            sql += ' FROM VOTERS WHERE 1';
+            if (name) {
+                if (m = /^([^,]+),\s*(.+)/.exec(name)) {
+                    sql += ' AND firstname LIKE ?';
+                    values.push(m[2] + '%');
+                    name = m[1];
+                }
+                sql += ' AND lastname LIKE ?';
+                values.push(name + '%');
+            }
+            if (address) {
+                if (m = /^(\d+)\s+(.+)/.exec(address)) {
+                    sql += ' AND res_house = ?';
+                    values.push(m[1]);
+                    address = m[2];
+                }
+                sql += ' AND res_street LIKE ?';
+                values.push(address + '%');
+            }
+            if (match) {
+                sql += ' AND ' + match + ' ORDER BY score DESC';
+                values.push(q);
+            }
+            else {
+                sql += ' ORDER BY lastname, firstname, middle, suffix DESC';
+            }
+            sql += ' LIMIT 10';
+            console.log(sql);
+            console.log(values);
+            db.query(sql, values, function (err, results) {
+                if (err) {
+                    throw err;
+                }
+                res.json(results);
+            });
         }
     };
 };
