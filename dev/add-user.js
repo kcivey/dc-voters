970e67c2fac34b3e81a5e1286c36d9468296ce05
Keith C. Ivey <keith@iveys.org>
2012-09-15 14:09:45 -0400

    improve add-user

--- dev/add-user.js
+++ dev/add-user.js
@@ -1,10 +1,11 @@
 var db = require('../db'),
     passwordHash = require('password-hash'),
+    _ = require('underscore'),
     table = 'users',
     username = process.argv[2],
     password = process.argv[3],
     pageRange = process.argv[4],
-    m, startPage, endPage;
+    m, startPage, endPage, digits;
 
 if (!username) {
     throw 'Missing username';
@@ -13,6 +14,18 @@ if (!/^\w+$/.test(username)) {
     throw 'Username must contain only letters, numbers, and underscores';
 }
 username = username.toLowerCase();
+
+if (/^\d+-\d+$/.test(password)) {
+    pageRange = password;
+    password = null;
+}
+
+if (!password) {
+    digits = _.range(10);
+    password = _.range(4).map(function () {
+        return _.range(10)[Math.floor(Math.random() * 10)].toString();
+    }).join('');
+}
 
 if (pageRange && (m = pageRange.match(/^(\d+)-(\d+)$/))) {
     startPage = +m[1];
@@ -24,9 +37,17 @@ db.query(
     [username, passwordHash.generate(password)],
     function (err, result) {
         if (err) {
-            throw err;
+            if (err.code == 'ER_DUP_ENTRY') {
+                console.log('User ' + username + ' already exists');
+            }
+            else {
+                throw err;
+            }
         }
-        console.log(result.affectedRows + ' user record created');
+        else {
+            console.log(result.affectedRows + ' user record created');
+            console.log('Username ' + username + ', password ' + password);
+        }
         if (pageRange) {
             db.query(
                 "UPDATE petition_lines SET checker = ? WHERE dcpt_code = '' AND page BETWEEN ? AND ?",
