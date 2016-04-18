ffd9822bd7f2e2e0a84fc9bfdeccf1b8d8d39c01
Keith C. Ivey <keith@iveys.org>
2016-04-17 22:45:18 -0400

    add pages and circulators tables

--- dev/create-table-users.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE users (
-  `id` MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(10),
-  `password` VARCHAR(64),
-  `email` VARCHAR(256),
-  `admin` TINYINT(1) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY (`username`)
-);
