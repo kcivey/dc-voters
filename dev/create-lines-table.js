f03f69b618ad91fbbbb8143ace1e43de408ede30
Keith C. Ivey <keith@iveys.org>
2016-04-20 00:30:10 -0400

    don't create records in create-lines-table.js

--- dev/create-lines-table.js
+++ dev/create-lines-table.js
@@ -5,8 +5,6 @@ var fs = require('fs'),
     table = 'petition_lines',
     sql = fs.readFileSync(path.join(__dirname, 'create-table-' + table + '.sql'), 'utf-8'),
     extraFields = JSON.parse(fs.readFileSync(path.join(path.dirname(__dirname), 'public', 'config.json'))).extraFields,
-    pages = 500,
-    linesPerPage = 20,
     extraSql = '';
 
 _.each(extraFields, function (field, code) {
@@ -16,24 +14,9 @@ _.each(extraFields, function (field, code) {
 sql = sql.replace(/(?=\n\s*PRIMARY KEY)/, extraSql);
 
 db.query(sql, function (err, rows) {
-    var values = [],
-        i, j;
     if (err) {
         throw err;
     }
     console.log('Created table ' + table);
-    sql = 'INSERT INTO ' + table + ' (page,line) VALUES ';
-    for (i = 1; i <= pages; i++) {
-        for (j = 1; j <= linesPerPage; j++) {
-            sql += (values.length ? ', ' : '') + '(?, ?)';
-            values.push(i, j);
-        }
-    }
-    db.query(sql, values, function (err, result) {
-        if (err) {
-            throw err;
-        }
-        console.log(result.message);
-        process.exit();
-    });
+    process.exit();
 });
