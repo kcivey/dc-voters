9462e1e52654f73416f760ec139fbc8f1b55be3b
Keith C. Ivey <keith@iveys.org>
2016-04-20 00:26:16 -0400

    restore create-table-petition_lines.sql because of extra fields

--- dev/create-tables.sql
+++ dev/create-tables.sql
@@ -14,29 +14,6 @@ CREATE TABLE IF NOT EXISTS circulators (
   PRIMARY KEY (`id`)
 );
 
-CREATE TABLE IF NOT EXISTS petition_lines (
-  `id` MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `page` SMALLINT UNSIGNED NOT NULL,
-  `line` TINYINT UNSIGNED NOT NULL,
-  `checker` VARCHAR(10) DEFAULT '' NOT NULL,
-  `check_time` DATETIME DEFAULT NULL,
-  `voter_id` VARCHAR(10) DEFAULT '' NOT NULL,
-  `finding` VARCHAR(10) DEFAULT '' NOT NULL,
-  `voter_name` VARCHAR(255) DEFAULT '' NOT NULL,
-  `address` VARCHAR(255) DEFAULT '' NOT NULL,
-  `ward` VARCHAR(1) DEFAULT '' NOT NULL,
-  `date_signed` DATE DEFAULT NULL,
-  `notes` TEXT DEFAULT '' NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `page_line` (`page`, `line`),
-  KEY (`checker`),
-  KEY (`check_time`),
-  KEY (`voter_id`),
-  KEY (`date_signed`),
-  KEY (`finding`),
-  KEY (`ward`)
-);
-
 CREATE TABLE IF NOT EXISTS users (
   `id` MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(10),
