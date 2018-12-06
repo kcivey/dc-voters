016fc107820907f74f3f823603c5e4fcf31f2d11
Keith C. Ivey <keith@iveys.org>
2018-12-05 19:36:34 -0500

    add connection string to db.js

--- db.js
+++ db.js
@@ -1,3 +1,11 @@
 var config = require('./config'),
-    mysql = require('mysql');
-module.exports = mysql.createConnection(config.get('database'));
+    mysql = require('mysql'),
+    dbConfig = config.get('database');
+
+module.exports = mysql.createConnection(dbConfig);
+module.exports.connectionString = 'mysql://' + encodeURIComponent(dbConfig.user) + ':' +
+    encodeURIComponent(dbConfig.password) + '@' + dbConfig.host +
+    '/' + dbConfig.database + '?timezone=' + dbConfig.timezone;
+if (dbConfig.dateStrings) {
+    module.exports.connectionString += 'dateStrings=true';
+}
