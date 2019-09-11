15551aca63eee22743802ca1e7f62f387a14ccf4
Keith C. Ivey <keith@iveys.org>
2019-09-10 22:39:49 -0400

    move getTotals() intp db.js

--- routes/index.js
+++ routes/index.js
@@ -49,77 +49,12 @@ module.exports = function (/* app */) {
         },
 
         getTotals(req, res) {
-            let sql = "SELECT (CASE WHEN c.status = '' THEN l.finding ELSE c.status END) AS combinedFinding, " +
-                'COUNT(*) AS count FROM petition_lines l ' +
-                'LEFT JOIN pages p ON l.project_id = p.project_id AND l.page = p.number ' +
-                'LEFT JOIN circulators c ON p.circulator_id = c.id ' +
-                'WHERE l.project_id = ? ';
-            const values = [req.project.id];
-            const circulatorId = +req.query.circulator;
-            if (circulatorId) {
-                sql += 'AND p.circulator_id = ? ';
-                values.push(circulatorId);
-            }
-            sql += 'GROUP BY combinedFinding';
-            db.query(sql, values, function (err, rows) {
+            db.getTotals(req.project.id, +req.query.circulator, function (err, results) {
                 if (err) {
                     console.error(err);
-                    res.sendStatus(500);
-                    return;
+                    return res.sendStatus(500);
                 }
-                const totals = {};
-                const wardBreakdown = {};
-                rows.forEach(function (row) {
-                    totals[row.combinedFinding] = +row.count;
-                });
-                db.query(
-                    // WHERE is because of bad data in BOE database (2 records with no ward)
-                    'SELECT ward, COUNT(*) as registered FROM voters WHERE ward > 0 GROUP BY ward',
-                    function (err, rows) {
-                        let totalRegistered = 0;
-                        if (err) {
-                            console.error(err);
-                            res.sendStatus(500);
-                            return;
-                        }
-                        rows.forEach(function (row) {
-                            wardBreakdown[row.ward] = {
-                                signers: 0,
-                                registered: row.registered,
-                            };
-                            totalRegistered += row.registered;
-                        });
-                        wardBreakdown.TOTAL = {
-                            signers: 0,
-                            registered: totalRegistered,
-                        };
-                        let sql = 'SELECT l.ward, COUNT(*) AS count FROM petition_lines l ';
-                        if (circulatorId) {
-                            sql += 'INNER JOIN pages p ON l.project_id = p.project_id AND l.page = p.number ';
-                        }
-                        sql += "WHERE l.finding = 'OK' AND l.project_id = ? ";
-                        const values = [req.project.id];
-                        if (circulatorId) {
-                            sql += 'AND p.circulator_id = ? ';
-                            values.push(circulatorId);
-                        }
-                        sql += 'GROUP BY l.ward';
-                        db.query(sql, values, function (err, rows) {
-                            if (err) {
-                                console.error(err);
-                                res.sendStatus(500);
-                                return;
-                            }
-                            rows.forEach(function (row) {
-                                if (wardBreakdown[row.ward]) {
-                                    wardBreakdown[row.ward].signers = row.count;
-                                    wardBreakdown.TOTAL.signers += row.count;
-                                }
-                            });
-                            res.json({totals, wardBreakdown});
-                        });
-                    }
-                );
+                return res.json(results);
             });
         },
 
