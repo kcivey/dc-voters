42f857fc4f786c5799834ad9e957e99ec9988ab1
Keith C. Ivey <keith@iveys.org>
2019-10-07 13:20:09 -0400

    improve create-database.js

--- dev/create-database.js
+++ dev/create-database.js
@@ -1,25 +1,89 @@
 #!/usr/bin/env node
 
+const assert = require('assert');
 const readline = require('readline');
+const {Writable} = require('stream');
+const yargs = require('yargs');
 const db = require('../lib/db/admin');
-const readlineInterface = readline.createInterface({input: process.stdin, output: process.stdout});
+const argv = getArgv();
 
 main()
     .catch(console.error)
     .finally(() => db.close());
 
 async function main() {
-    const adminUser = await ask('Admin user [admin]: ') || 'admin';
-    const adminPassword = await ask('Admin password: ');
-    const database = await ask('Database name [dc_voters]: ');
+    const adminUser = argv['admin-user'];
+    const adminPassword = argv['admin-password'] || await askPassword('Admin password: ');
+    const {database, user, password} = argv;
     db.establishConnection({database: '', user: adminUser, password: adminPassword});
-    const user = await ask(`Username [${database}]: `);
-    const password = await ask('Password: [(random)]: ');
     const opts = await db.createDatabaseAndUser(database, user, password);
-    readlineInterface.close();
     console.log(opts);
 }
 
-function ask(question) {
-    return new Promise(resolve => readlineInterface.question(question, resolve));
+function askPassword(question) {
+    process.stdout.write('MySQL admin password: ');
+    const mutedStdout = new Writable({
+        write: (chunk, encoding, callback) => callback(),
+    });
+    const readlineInterface = readline.createInterface({
+        input: process.stdin,
+        output: mutedStdout,
+        terminal: true,
+    });
+    return new Promise(resolve => readlineInterface.question(question, resolve))
+        .then(function (password) {
+            readlineInterface.close();
+            return password;
+        });
+}
+
+function getArgv() {
+    return yargs
+        .options({
+            'admin-user': {
+                type: 'string',
+                describe: 'username for MySQL account that can create database',
+                default: 'admin',
+                requiresArg: true,
+            },
+            'admin-password': {
+                type: 'string',
+                describe: 'password for MySQL account that can create database (will prompt if not supplied)',
+                requiresArg: true,
+            },
+            database: {
+                type: 'string',
+                describe: 'name for database',
+                required: true,
+                requiresArg: true,
+            },
+            user: {
+                type: 'string',
+                describe: 'username for MySQL account to create (defaults to database name)',
+                requiresArg: true,
+            },
+            password: {
+                type: 'string',
+                describe: 'password for MySQL account to create (generated randomly if not supplied)',
+                requiresArg: true,
+            },
+        })
+        .usage('$0 <database>', 'create database', function (yargs) {
+            yargs.positional('database', {
+                type: 'string',
+                describe: 'name for database to create',
+            });
+        })
+        .check(function (argv) {
+            assert.strictEqual(argv._.length, 0, 'Unexpected arguments after database name');
+            return true;
+        })
+        .middleware(function (argv) {
+            if (!argv.user) {
+                argv.user = argv.database;
+            }
+            return true;
+        })
+        .strict(true)
+        .argv;
 }
