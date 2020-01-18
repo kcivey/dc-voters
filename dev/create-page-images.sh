aaf0deb641014fa1922298a9a53ba53c92dd6035
Keith C. Ivey <keith@iveys.org>
2020-01-18 11:04:27 -0500

    modify create-page-images.sh to handle one PDF per page

--- dev/create-page-images.sh
+++ dev/create-page-images.sh
@@ -5,6 +5,13 @@ typeset -i PAGES P I
 P=1
 for PDF in "$@"
 do
+    echo $PDF
+    if [[ $PDF =~ ([0-9]+) ]]
+    then
+        P=$(( (${BASH_REMATCH[1]} * 2) - 1 ))
+    else
+        echo "No number in $PDF"
+    fi
     PAGES=$(pdfinfo $PDF | grep Pages | awk '{ print $2 }')
     for (( I = 1; I <= PAGES; I++ ))
     do
