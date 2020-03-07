7aa93a73412577826897d0665e3b6ac032879a31
Keith C. Ivey <keith@iveys.org>
2020-03-07 16:37:37 -0500

    add --flip-back option to create-page-images.sh

--- dev/create-page-images.sh
+++ dev/create-page-images.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
 set -e
-typeset -i PAGES P I
+typeset -i PAGES P I FLIP_BACK
 P=1
+FLIP_BACK=0
 for PDF in "$@"
 do
     echo $PDF
+    if [[ $PDF = '--flip-back' ]]
+    then
+        echo 'Flip back sides of pages'
+        FLIP_BACK=1
+        continue
+    fi
     if [[ $PDF =~ ([0-9]+) ]]
     then
         P=$(( (${BASH_REMATCH[1]} * 2) - 1 ))
@@ -18,7 +25,16 @@ do
         echo $I
         F=$(printf '../public/page-images/%04d%s.jpeg' $(( (P + 1) / 2 )) $(if (( $P % 2 )); then echo 'a'; else echo 'b'; fi))
         echo $F
-        [[ -e $F ]] || convert -density 200 $PDF\[$[$I - 1]\] -strip +profile '*' -interlace plane -gaussian-blur 0.05 -quality 75% $F
+        if [[ ! -e $F ]]
+        then
+            convert -density 200 $PDF\[$[$I - 1]\] -strip +profile '*' -interlace plane -gaussian-blur 0.05 -quality 75% $F
+            if (( (P % 2) && $FLIP_BACK ))
+            then
+                echo 'Flipping'
+                convert $F -rotate 180 temp.jpeg
+                mv temp.jpeg $F
+            fi
+        fi
         let P++
     done;
 done
