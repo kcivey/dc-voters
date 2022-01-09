8b76b4f9c6d096fdcb3692ff875dd0a0f341f7cb
Keith C. Ivey <keith@iveys.org>
2022-01-09 12:39:11 -0500

    export mailing

--- misc/z-mailing.js
+++ misc/z-mailing.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 
+const assert = require('assert');
+const fs = require('fs');
+const csvParse = require('csv-parse');
+const stringify = require('csv-stringify/lib/sync');
 const db = require('../lib/db');
 
 main()
@@ -7,21 +11,28 @@ main()
     .finally(() => db.close());
 
 async function main() {
-    const records = await db.queryPromise(
-        `SELECT *
-        FROM zottoli
-        WHERE street_address <> '' AND fixed_address IS NULL`
-    );
-    for (const r of records) {
-        const address = r.street_address
-            .replace(/^(\d+) .* (\1 .*)$/, '$2')
-            .replace(/ Stre(?:et|ss) (?=[NS][EW])/, ' St ')
-            .replace(/ Circle (?=[NS][EW])/, ' Cir ');
-        await db.queryPromise(
-            `UPDATE zottoli
-            SET fixed_address = ?
-            WHERE first_name = ? AND last_name = ? AND middle = ? AND street_address = ?`,
-            [address, r.first_name, r.last_name, r.middle, r.street_address]
+    const infile = process.argv[2];
+    assert(infile, 'Input file required');
+    const parser = fs.createReadStream(infile)
+        .pipe(csvParse({columns: true}));
+    const projectId = (await db.getProjectByCode('i82')).id;
+    let headers = true;
+    for await (const r of parser) {
+        if (headers) {
+            process.stdout.write(stringify([Object.keys(r)]));
+            headers = false;
+        }
+        const signed = await (
+            db.queryPromise(
+                `SELECT voter_id FROM petition_lines
+                WHERE project_id = ?
+                    AND voter_id = ?
+                    AND finding = ?`,
+                [projectId, r.new_voter_id, 'OK']
+            ).then(rows => rows.length > 0)
         );
+        if (!signed) {
+            process.stdout.write(stringify([r]));
+        }
     }
 }
